929ae398737353914609d1ceb98929002d33b803
JIN HONG KIM <77311928+chrysanthemum0@users.noreply.github.com>
2024-05-02 17:23:51 +0900

    Add files via upload

---
+++ storage/framework/views/e89f7d59ae3aa9b93daced1ea4f2622cbb460d4f.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+<h1>Rename</h1>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\student\Desktop\laravel-kadai\message-board\resources\views/home/edit.blade.php ENDPATH**/ ?>
