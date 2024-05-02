19d1b2b743e6d7a8d64e5595c537a08e51f490d3
JIN HONG KIM <77311928+chrysanthemum0@users.noreply.github.com>
2024-05-02 17:48:53 +0900

    Delete message-board directory

--- message-board/storage/framework/views/a056ab30aa8901755170bf886406dbac21f523dc.php
+++
@@ -1,8 +0,0 @@
-<?php if($errors->count()): ?>
-    <ul class="alert">
-        <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-            <li><?php echo e($error); ?></li>
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-    </ul>
-<?php endif; ?>
-<?php /**PATH C:\Users\student\Desktop\laravel-kadai\message-board\resources\views/commons/flash.blade.php ENDPATH**/ ?>
